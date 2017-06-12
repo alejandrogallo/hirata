abef831a9017adb7dc1af24f7ecf17bc4c086658
Alejandro Gallo <aamsgallo@gmail.com>
2017-06-12 11:26:54 +0200

    H21 classified

--- eom-ccsd/ccd-eomd/L1_manual.cpp
+++ eom-ccsd/ccd-eomd/L1_manual.cpp
@@ -1,5 +1,5 @@
-( - 0.5 )  *  Vabij["jclm"] * Labij["mlca"]
-( - 0.5 )  *  Vabij["cdma"] * Labij["mjdc"]
+( - 0.5 )  *  Vabij["jclm"] * Labij["mlca"] // H21 (mlc) -> aj
+( - 0.5 )  *  Vabij["cdma"] * Labij["mjdc"] // H21 (mdc) -> aj
 
 // These are zero because we have hartree fock orbitals
 //( + 1.0 )  *  Fai["cl"]     * Labij["ljca"]
@@ -7,11 +7,12 @@
 //( + 0.5 )  *  Fai["ka"]     * Tabij["denk"] * Labij["njed"]
 //( + 1.0 )  *  Fai["kd"]     * Tabij["ednk"] * Labij["njea"]
 
-( - 0.5 )  *  Tabij["cdmn"] * Vabij["njoa"] * Labij["omdc"]
-( - 1.0 )  *  Tabij["cdmn"] * Vabij["njod"] * Labij["omca"]
-( - 0.25 ) *  Tabij["cdmn"] * Vabij["mnoa"] * Labij["ojdc"]
-( - 0.5 )  *  Tabij["cdmn"] * Vabij["mnod"] * Labij["ojca"]
-( - 0.5 )  *  Tabij["cdmn"] * Vabij["jgda"] * Labij["nmgc"]
-( - 0.25 ) *  Tabij["cdmn"] * Vabij["jgcd"] * Labij["nmga"]
-( - 1.0 )  *  Tabij["cdmn"] * Vabij["ngda"] * Labij["mjgc"]
-( - 0.5 )  *  Tabij["cdmn"] * Vabij["ngcd"] * Labij["mjga"]
+( - 0.5 )  *  Tabij["cdmn"] * Vabij["njoa"] * Labij["omdc"] // H21 (mncdo) -> aj
+( - 1.0 )  *  Tabij["cdmn"] * Vabij["njod"] * Labij["omca"] // H21 (mncdo) -> aj
+( - 0.25 ) *  Tabij["cdmn"] * Vabij["mnoa"] * Labij["ojdc"] // H21 (mncdo) -> aj
+( - 0.5 )  *  Tabij["cdmn"] * Vabij["mnod"] * Labij["ojca"] // H21 (mncdo) -> aj
+( - 0.5 )  *  Tabij["cdmn"] * Vabij["jgda"] * Labij["nmgc"] // H21 (mncdg) -> aj
+( - 0.25 ) *  Tabij["cdmn"] * Vabij["jgcd"] * Labij["nmga"] // H21 (mncdg) -> aj
+( - 1.0 )  *  Tabij["cdmn"] * Vabij["ngda"] * Labij["mjgc"] // H21 (mncdg) -> aj
+( - 0.5 )  *  Tabij["cdmn"] * Vabij["ngcd"] * Labij["mjga"] // H21 (mncdg) -> aj
+
