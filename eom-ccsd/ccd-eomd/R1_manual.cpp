42c19026298fe556e05b9b8d1394dcbc746bd339
Alejandro Gallo <aamsgallo@gmail.com>
2017-06-12 11:28:59 +0200

    Classifies R1 manual

--- eom-ccsd/ccd-eomd/R1_manual.cpp
+++ eom-ccsd/ccd-eomd/R1_manual.cpp
@@ -1,5 +1,6 @@
 // These are zero because we have hartree fock orbitals
 //( + 1.0 ) *  Fai["kd"]     * Rabij["dbki"]
 
-( + 0.5 ) *  Vabij["klie"] * Rabij["ebkl"]
-( + 0.5 ) *  Vabij["kbde"] * Rabij["deki"]
+( + 0.5 ) *  Vabij["klie"] * Rabij["ebkl"]  // H21 (kle) -> bi
+( + 0.5 ) *  Vabij["kbde"] * Rabij["deki"]  // H21 (dek) -> bi
+
