1d76bab872754ded19139c8c1a76f5cb4298df9a
Alejandro Gallo <aamsgallo@gmail.com>
2017-06-12 17:20:29 +0200

    Add Labij

--- eom-ccsd/ccd-eomd/LHR.cpp
+++ eom-ccsd/ccd-eomd/LHR.cpp
@@ -4,15 +4,15 @@
 ( + 0.5 ) * Lai["ib"] *  Vabij["kbde"] * Rabij["deki"]  // H21 (dek) -> bi
 
 // R2
-( - 1.0 + 1.0 * P("cdij", "cdji") )     *  Fai["mi"]              * Rabij["cdmj"] // m -> cdij
-( - 1.0 + 1.0 * P("dcij", "cdij") )     *  Fai["de"]              * Rabij["ecij"] // e -> dcij
-( + 0.5 )     *  Vabij["mnij"]          * Rabij["cdmn"] // mn -> cdij
-( + 1.0 - 1.0 * P("dcij", "cdij") - 1.0 * P("dcij", "dcji") + 1.0 * P("dcij", "cdji") ) *  Vabij["mdif"] * Rabij["fcmj"] //mf dcij
-( + 0.5 )     *  Vabij["cdef"]          * Rabij["efij"] // ef -> cdij
-( + 0.5 - 0.5 * P("dcij", "cdij") )     *  Tabij["edij"]          * Vabij["noeh"]       * Rabij["hcno"] // ohne -> cdij
-( + 0.25 )    *  Tabij["efij"]          * Vabij["opef"]           * Rabij["cdop"] // efop -> cdij
-( - 0.5 + 0.5 * P("cdij", "cdji") )     *  Tabij["cdmi"]          * Vabij["mngh"]       * Rabij["ghnj"] // mngh -> cdij
-( - 1.0 + 1.0 * P("dcij", "cdij") + 1.0 * P("dcij", "dcji") - 1.0 * P("dcij", "cdji") ) *  Tabij["edni"] * Vabij["noeh"] * Rabij["hcoj"] // enho -> cdij
-( - 0.5 + 0.5 * P("cdij", "cdji") )     *  Tabij["efoi"]          * Vabij["opef"]       * Rabij["cdpj"] // efop -> cdij
-( + 0.25 )    *  Tabij["cdmn"]          * Vabij["mngh"]           * Rabij["ghij"] // mngh -> cdij
-( + 0.5 - 0.5 * P("dcij", "cdij") )     *  Tabij["edno"]          * Vabij["noeh"]       * Rabij["hcij"] // noeh -> cdij
+( - 1.0 + 1.0 * P("cdij", "cdji") )     * Labij ["ijcd"]          * Fai["mi"]                   * Rabij["cdmj"] // m -> cdij
+( - 1.0 + 1.0 * P("dcij", "cdij") )     * Labij["ijdc"]           * Fai["de"]                   * Rabij["ecij"] // e -> dcij
+( + 1.0 - 1.0 * P("dcij", "cdij") - 1.0 * P("dcij", "dcji") + 1.0 * P("dcij", "cdji") )         * Labij["ijcd"]                 * Vabij["mdif"]                 * Rabij["fcmj"] //mf dcij
+( + 0.5 - 0.5 * P("dcij", "cdij") )     * Labij["ijcd"]           * Tabij["edij"]               * Vabij["noeh"]                 * Rabij["hcno"] // ohne -> cdij
+( - 0.5 + 0.5 * P("cdij", "cdji") )     * Labij["ijcd"]           * Tabij["cdmi"]               * Vabij["mngh"]                 * Rabij["ghnj"] // mngh -> cdij
+( - 1.0 + 1.0 * P("dcij", "cdij") + 1.0 * P("dcij", "dcji") - 1.0 * P("dcij", "cdji") )         * Labij["ijcd"]                 * Tabij["edni"]                 * Vabij["noeh"]           * Rabij["hcoj"] // enho -> cdij
+( - 0.5 + 0.5 * P("cdij", "cdji") )     * Labij["ijcd"]           * Tabij["efoi"]               * Vabij["opef"]                 * Rabij["cdpj"] // efop -> cdij
+( + 0.5 - 0.5 * P("dcij", "cdij") )     * Labij["ijcd"]           * Tabij["edno"]               * Vabij["noeh"]                 * Rabij["hcij"] // noeh -> cdij
+( + 0.5 )     * Labij["ijcd"]           * Vabij["mnij"]           * Rabij["cdmn"] // mn -> cdij
+( + 0.5 )     * Labij["ijcd"]           * Vabij["cdef"]           * Rabij["efij"] // ef -> cdij
+( + 0.25 )    * Labij["ijcd"]           * Tabij["efij"]           * Vabij["opef"]               * Rabij["cdop"] // efop -> cdij
+( + 0.25 )    * Labij["ijcd"]           * Tabij["cdmn"]           * Vabij["mngh"]               * Rabij["ghij"] // mngh -> cdij
