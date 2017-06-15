ea9843df9de034f9893ffe6d56cdf7d9f6a74d7e
Alejandro Gallo <aamsgallo@gmail.com>
2017-06-15 15:42:07 +0200

    Add Vabcd

--- eom-ccsd/process_input.sh
+++ eom-ccsd/process_input.sh
@@ -119,6 +119,7 @@ for inf in ${in_files[@]} ; do
     /Vabij/s/Vabij\[\(\"$I$I$A$A\"\)\]/Vijab[\1]/g;
     /Vabij/s/Vabij\[\(\"$I$I$I$A\"\)\]/Vijka[\1]/g;
     /Vabij/s/Vabij\[\(\"$A$A$I$A\"\)\]/Vabic[\1]/g;
+    /Vabij/s/Vabij\[\(\"$A$A$A$A\"\)\]/Vabcd[\1]/g;
     /Vabij/s/Vabij\[\(\"$I$A$A$A\"\)\]/Viabc[\1]/g;
   }
   " ${out}
