f278109c740d540567b2d05dbc225bead4cba5a1
Alejandro Gallo <aamsgallo@gmail.com>
2017-07-05 15:51:16 +0200

    Add tests for permute_indices function

---
+++ tests/test_permute_indices.py
@@ -0,0 +1,7 @@
+from hirata import permute_indices
+
+assert permute_indices("ab", "ib", "bi") == "ai"
+assert permute_indices("abcd", "cd", "dc") == "abdc"
+assert permute_indices("abcd", "cd", "ij") == "abij"
+
+
