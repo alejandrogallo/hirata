b3ec379bafa41f86eb8106e01cca53bf5075ae4c
Alejandro Gallo <aamsgallo@gmail.com>
2017-07-06 15:41:02 +0200

    Update test

--- tests/test_create_tensor_with_indices.py
+++ tests/test_create_tensor_with_indices.py
@@ -1,3 +1,19 @@
 from hirata import get_tensor_name_with_indices
 
 assert get_tensor_name_with_indices("T", "abij", "ocdm") == "Tabij[\"cdmo\"]"
+assert get_tensor_name_with_indices("T", "aibj", "ocdm") == "Taibj[\"cmdo\"]"
+assert get_tensor_name_with_indices("T", "aijb", "odmc") == "Taijb[\"cmod\"]"
+
+try:
+    get_tensor_name_with_indices("T", "ab", "ij")
+except SystemExit:
+    assert True
+else:
+    assert False
+
+try:
+    get_tensor_name_with_indices("T", "ab", "abc")
+except SystemExit:
+    assert True
+else:
+    assert False
