6224c1f8930f1ec0e5de0b597f12a8026f22dae8
Alejandro Gallo <aamsgallo@gmail.com>
2017-07-06 16:22:31 +0200

    Update contracted script

--- eom-ccsd/create-contracted.sh
+++ eom-ccsd/create-contracted.sh
@@ -4,10 +4,15 @@ hirata=../hirata.py
 
 set -x
 
-${hirata} -f L1.in -o L1HR1_from_L1.cpp --prepend 'energy[""] = ' --contract-with R --with-indices ai   &&
-${hirata} -f L2.in -o L2HR2_from_L2.cpp --prepend 'energy[""] = ' --contract-with R --with-indices abij &&
-${hirata} -f R1.in -o L1HR1_from_R1.cpp --prepend 'energy[""] = ' --contract-with L --with-indices ia   &&
-${hirata} -f R2.in -o L2HR2_from_R2.cpp --prepend 'energy[""] = ' --contract-with L --with-indices ijab
+${hirata} --fock -f L1.in -o L1HR1_from_L1.cpp --prepend 'energy[""] = ' --contract-with R --with-indices ai   &&
+${hirata} --fock -f L2.in -o L2HR2_from_L2.cpp --prepend 'energy[""] = ' --contract-with R --with-indices abij &&
+${hirata} --fock -f R1.in -o L1HR1_from_R1.cpp --prepend 'energy[""] = ' --contract-with L --with-indices ia   &&
+${hirata} --fock -f R2.in -o L2HR2_from_R2.cpp --prepend 'energy[""] = ' --contract-with L --with-indices ijab
 
+cat L1HR1_from_L1.cpp L2HR2_from_L2.cpp > LHR_from_L.cpp
+cat L1HR1_from_R1.cpp L2HR2_from_R2.cpp > LHR_from_R.cpp
+
+sed "/\/\//d; /^$/d" LHR_from_L.cpp > LHR_from_L_clean.cpp
+sed "/\/\//d; /^$/d" LHR_from_R.cpp > LHR_from_R_clean.cpp
 
 #vim-run: bash %
