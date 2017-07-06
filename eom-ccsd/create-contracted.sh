33e3ba64986646b61eaf529747ec501833b405a5
Alejandro Gallo <aamsgallo@gmail.com>
2017-07-06 16:09:01 +0200

    Add script to create contracted equations

---
+++ eom-ccsd/create-contracted.sh
@@ -0,0 +1,13 @@
+#! /usr/bin/env bash
+
+hirata=../hirata.py
+
+set -x
+
+${hirata} -f L1.in -o L1HR1_from_L1.cpp --contract-with R --with-indices ai   &&
+${hirata} -f L2.in -o L2HR2_from_L2.cpp --contract-with R --with-indices abij &&
+${hirata} -f R1.in -o L1HR1_from_R1.cpp --contract-with L --with-indices ia   &&
+${hirata} -f R2.in -o L2HR2_from_R2.cpp --contract-with L --with-indices ijab
+
+
+#vim-run: bash %
