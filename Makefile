74cc71268837638ea75b0a53ef797e3edb8e346d
Alejandro Gallo <aamsgallo@gmail.com>
2017-07-06 20:06:30 +0200

    add contracted

--- Makefile
+++ Makefile
@@ -9,7 +9,8 @@ eom_ccsd_SOURCES = $(wildcard eom-ccsd/*.in)
 eom_ccsd_TARGETS = \
 $(patsubst %.in,%.cpp,$(eom_ccsd_SOURCES)) \
 $(patsubst %.in,%-fock.cpp,$(eom_ccsd_SOURCES)) \
-$(patsubst %.in,%-uncomment-fock.cpp,$(eom_ccsd_SOURCES))
+$(patsubst %.in,%-uncomment-fock.cpp,$(eom_ccsd_SOURCES)) \
+eom-ccsd/contracted
 
 eom_ccd_SOURCES = $(patsubst eom-ccsd%,eom-ccd%,$(eom_ccsd_SOURCES))
 eom_ccd_TARGETS = \
@@ -24,6 +25,10 @@ eom-ccd/%.in: eom-ccsd/%.in
 	@echo Creating $@ from $<
 	@mkdir -p $(dir $@)
 	sed "/t\s*(\s*[ph][0-9]\s\+[ph][0-9]\s*)/d" $< > $@
+
+%/contracted:
+	mkdir -p $@
+	tools/create-contracted-eom.sh $*
 
 eom-ccd: $(eom_ccd_TARGETS) ## Create eom-ccd equations
 
--- tools/create-contracted-eom.sh
+++ tools/create-contracted-eom.sh
@@ -16,29 +16,31 @@ fi
 equations_folder=$1
 cd ${equations_folder}
 
+folder=contracted
+
 set -x
 
 ${hirata} --fock \
   -f L1.in \
-  -o L1HR1_from_L1.cpp \
+  -o ${folder}/L1HR1_from_L1.cpp \
   --prepend 'energy[""] = ' --contract-with R --with-indices ai   &&
 ${hirata} --fock \
   -f L2.in \
-  -o L2HR2_from_L2.cpp \
+  -o ${folder}/L2HR2_from_L2.cpp \
   --prepend 'energy[""] = ' --contract-with R --with-indices abij &&
 ${hirata} --fock \
   -f R1.in \
-  -o L1HR1_from_R1.cpp \
+  -o ${folder}/L1HR1_from_R1.cpp \
   --prepend 'energy[""] = ' --contract-with L --with-indices ia   &&
 ${hirata} --fock \
   -f R2.in \
-  -o L2HR2_from_R2.cpp \
+  -o ${folder}/L2HR2_from_R2.cpp \
   --prepend 'energy[""] = ' --contract-with L --with-indices ijab
 
-cat L1HR1_from_L1.cpp L2HR2_from_L2.cpp > LHR_from_L.cpp
-cat L1HR1_from_R1.cpp L2HR2_from_R2.cpp > LHR_from_R.cpp
+cat ${folder}/L1HR1_from_L1.cpp ${folder}/L2HR2_from_L2.cpp > ${folder}/LHR_from_L.cpp
+cat ${folder}/L1HR1_from_R1.cpp ${folder}/L2HR2_from_R2.cpp > ${folder}/LHR_from_R.cpp
 
-sed "/\/\//d; /^$/d" LHR_from_L.cpp > LHR_from_L_clean.cpp
-sed "/\/\//d; /^$/d" LHR_from_R.cpp > LHR_from_R_clean.cpp
+sed "/\/\//d; /^$/d" ${folder}/LHR_from_L.cpp > ${folder}/LHR_from_L_clean.cpp
+sed "/\/\//d; /^$/d" ${folder}/LHR_from_R.cpp > ${folder}/LHR_from_R_clean.cpp
 
 #vim-run: bash %
