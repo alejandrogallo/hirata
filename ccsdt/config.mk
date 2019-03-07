7c2130ab29367d122b46a21fe64f6bdb2c31d78d
Alejandro Gallo <aamsgallo@gmail.com>
2019-03-07 11:44:28 +0100

    Add prepend sources

--- ccsdt/config.mk
+++ ccsdt/config.mk
@@ -1,8 +1,21 @@
 ccsdt_SOURCES = $(wildcard ccsdt/*.in)
 ccsdt_TARGETS = \
 $(patsubst %.in,%.cpp,$(ccsdt_SOURCES)) \
+$(patsubst %.in,%-prepend.cpp,$(ccsdt_SOURCES)) \
 
 ccsdt: $(ccsdt_TARGETS) ## Create ccsdt equations
 
 clean-ccsdt:
 	-@rm -v $(ccsdt_TARGETS)
+
+ccsdt/E-prepend.cpp: ccsdt/E.in
+	hirata -f $< -o $@ --prepend 'energy[""] = '
+
+ccsdt/T1-prepend.cpp: ccsdt/T1.in
+	hirata -f $< -o $@ --prepend '(*Rai)["bi"] = '
+
+ccsdt/T2-prepend.cpp: ccsdt/T2.in
+	hirata -f $< -o $@ --prepend '(*Rabij)["cdij"] = '
+
+ccsdt/T3-prepend.cpp: ccsdt/T3.in
+	hirata -f $< -o $@ --prepend '(*Rabcijk)["edfijk"] = '
