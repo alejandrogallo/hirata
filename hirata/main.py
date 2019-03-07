a057b7fd3f90ecca74c5d532b09ed9852c678df2
Alejandro Gallo <aamsgallo@gmail.com>
2019-03-07 11:22:44 +0100

    lint

--- hirata/main.py
+++ hirata/main.py
@@ -80,47 +80,54 @@ def main():
         description=""
     )
 
-    parser.add_argument("-f", "--file",
-        help="Input file",
-        action="store"
-    )
+    parser.add_argument("-f", "--file", help="Input file", action="store")
 
-    parser.add_argument("-o", "--out",
+    parser.add_argument(
+        "-o", "--out",
         help="Output file (hirata.out)",
         default="hirata.cpp",
         action="store"
     )
 
-    parser.add_argument("--no-comments",
+    parser.add_argument(
+        "--no-comments",
         help="Strip out the comments",
         action="store_true"
     )
 
-    parser.add_argument("--contract-with",
+    parser.add_argument(
+        "--contract-with",
         help="The name of the operator you want to contract with",
         action="store"
     )
 
-    parser.add_argument("--with-indices",
-        help="The indices of the operator that you want, for example abij etc...",
+    parser.add_argument(
+        "--with-indices",
+        help=(
+            "The indices of the operator that you want,"
+            " for example abij etc..."
+        ),
         default=None,
         action="store"
     )
 
-    parser.add_argument("--prepend",
+    parser.add_argument(
+        "--prepend",
         help="Prepend anything to every line of the equations",
         default="",
         action="store"
     )
 
-    parser.add_argument("--with-intermediates",
+    parser.add_argument(
+        "--with-intermediates",
         help="""Put every line equal to some intermediate, you need to provide
         a name""",
         default="",
         action="store"
     )
 
-    parser.add_argument("--python-tuples-out",
+    parser.add_argument(
+        "--python-tuples-out",
         help="Export data in python tuples to a file",
         default="",
         action="store"
