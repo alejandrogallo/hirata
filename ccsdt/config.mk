f0de66c092c6b9928d77e41263214c3b8514bfb8
Alejandro Gallo <aamsgallo@gmail.com>
2019-03-07 11:55:20 +0100

    No comment

--- ccsdt/config.mk
+++ ccsdt/config.mk
@@ -2,6 +2,7 @@ ccsdt_SOURCES = $(wildcard ccsdt/*.in)
 ccsdt_TARGETS = \
 $(patsubst %.in,%.cpp,$(ccsdt_SOURCES)) \
 $(patsubst %.in,%-prepend.cpp,$(ccsdt_SOURCES)) \
+$(patsubst %.in,%-prepend-nocomment.cpp,$(ccsdt_SOURCES)) \
 
 ccsdt: $(ccsdt_TARGETS) ## Create ccsdt equations
 
@@ -10,12 +11,12 @@ clean-ccsdt:
 
 ccsdt/E-prepend.cpp: ccsdt/E.in
 	hirata -f $< -o $@ --prepend 'energy[""] = '
-
 ccsdt/T1-prepend.cpp: ccsdt/T1.in
 	hirata -f $< -o $@ --prepend '(*Rai)["bi"] = '
-
 ccsdt/T2-prepend.cpp: ccsdt/T2.in
 	hirata -f $< -o $@ --prepend '(*Rabij)["cdij"] = '
-
 ccsdt/T3-prepend.cpp: ccsdt/T3.in
 	hirata -f $< -o $@ --prepend '(*Rabcijk)["edfijk"] = '
+
+%-nocomment.cpp: %.cpp
+	sed "/^\/\//d; /^\$$/d;" $< > $@
