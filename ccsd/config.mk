daeed4115d79f5bd547a8f9c560275497a469a5c
Alejandro Gallo <aamsgallo@gmail.com>
2019-03-08 18:44:24 +0100

    Update ccsd

--- ccsd/config.mk
+++ ccsd/config.mk
@@ -1,12 +1,21 @@
 ccsd_SOURCES = $(wildcard ccsd/*.in)
 ccsd_TARGETS = \
 $(patsubst %.in,%.cpp,$(ccsd_SOURCES)) \
+$(patsubst %.in,%.cpp,$(ccsd_SOURCES)) \
+$(patsubst %.in,%-prepend.cpp,$(ccsd_SOURCES)) \
+$(patsubst %.in,%-prepend-nocomment.cpp,$(ccsd_SOURCES)) \
 
 ccsd: $(ccsd_TARGETS) ## Create ccsd equations
 
 clean-ccsd:
 	-@rm -v $(ccsd_TARGETS)
 
-ccsd/intermediates:
-	mkdir -p $@
-	ccsd/scripts/create-intermediates.sh ccsd $(FD_OUTPUT)
+ccsd/E-prepend.cpp: ccsd/E.in
+	hirata -f $< -o $@ --prepend 'energy[""] = '
+ccsd/T1-prepend.cpp: ccsd/T1.in
+	hirata -f $< -o $@ --prepend '(*Rai)["bi"] = '
+ccsd/T2-prepend.cpp: ccsd/T2.in
+	hirata -f $< -o $@ --prepend '(*Rabij)["cdij"] = '
+
+%-nocomment.cpp: %.cpp
+	sed "/^\/\//d; /^\$$/d;" $< > $@
