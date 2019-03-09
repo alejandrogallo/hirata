330c1f7ee5f61c9a58c6900542b8b742319c8d75
Alejandro Gallo <aamsgallo@gmail.com>
2019-03-09 02:34:21 +0100

    Add more cisd

--- tests/test_cisd.py
+++ tests/test_cisd.py
@@ -46,3 +46,71 @@ def test_E():
         cl_doubles.to_cpp() == ['( + 0.25  ) * Tabij["abkl"] * Vijab["klab"];']
     )
 
+
+def test_T2():
+    """
+    [ + 1.0 ] * v ( p3 p4 h1 h2 )
+    [ - 1.0 + 1.0 * P( p4 p3 h1 h2 => p3 p4 h1 h2 ) + 1.0 * P( p4 p3 h1 h2 => p4 p3 h2 h1 ) - 1.0 * P( p4 p3 h1 h2 => p3 p4 h2 h1 ) ] * f ( p4 h1 ) * t ( p3 h2 )
+    [ - 1.0 + 1.0 * P( p3 p4 h1 h2 => p4 p3 h1 h2 ) ] * Sum ( h5 ) * t ( p3 h5 ) * v ( h5 p4 h1 h2 )
+    [ + 1.0 - 1.0 * P( p3 p4 h2 h1 => p3 p4 h1 h2 ) ] * Sum ( p5 ) * t ( p5 h2 ) * v ( p3 p4 h1 p5 )
+    [ - 1.0 + 1.0 * P( p3 p4 h1 h2 => p3 p4 h2 h1 ) ] * Sum ( h5 ) * f ( h5 h1 ) * t ( p3 p4 h5 h2 )
+    [ - 1.0 + 1.0 * P( p4 p3 h1 h2 => p3 p4 h1 h2 ) ] * Sum ( p5 ) * f ( p4 p5 ) * t ( p5 p3 h1 h2 )
+    [ + 0.5 ] * Sum ( h5 h6 ) * t ( p3 p4 h5 h6 ) * v ( h5 h6 h1 h2 )
+    [ + 1.0 - 1.0 * P( p3 p4 h2 h1 => p4 p3 h2 h1 ) - 1.0 * P( p3 p4 h2 h1 => p3 p4 h1 h2 ) + 1.0 * P( p3 p4 h2 h1 => p4 p3 h1 h2 ) ] * Sum ( h6 p5 ) * t ( p5 p3 h6 h2 ) * v ( h6 p4 h1 p5 )
+    [ + 0.5 ] * Sum ( p5 p6 ) * t ( p5 p6 h1 h2 ) * v ( p3 p4 p5 p6 )
+    [ - 1.0 ] * e ( ) * t ( p3 p4 h1 h2 )
+    """
+    infile = "cisd/T2.in"
+    assert(os.path.exists(infile))
+    with open(infile) as f:
+        lines = f.readlines()
+    assert(len(lines) == 10)
+
+    hl1 = HirataLine(lines[0])
+    assert(set(hl1.free_indices.split()) == set("p3 p4 h1 h2".split()))
+    assert(hl1.summation_indices == '')
+    assert(hl1.prefactors == ["+ 1.0 "])
+    assert(hl1.postfactors == ['v ( p3 p4 h1 h2 )'])
+    assert(str(hl1) == lines[0].replace('\n', ''))
+    cl1 = Cc4sLine(hl1)
+    assert(set(cl1.free_indices.split()) == set(["c", "d", "i", "j"]))
+    assert(cl1.summation_indices == "")
+    assert(cl1.prefactors == ["+ 1.0 "])
+    assert(cl1.postfactors == ['Vabij["cdij"]'])
+    assert(cl1.to_cpp() == ['( + 1.0  ) * Vabij["cdij"];'])
+
+    hl8 = HirataLine(lines[7])
+    assert(set(hl8.free_indices.split()) == set("p3 p4 h1 h2".split()))
+    assert(hl8.summation_indices == ' h6 p5 ')
+    assert(
+        hl8.prefactors == [
+            '+ 1.0 ',
+            '- 1.0 * P( p3 p4 h2 h1 => p4 p3 h2 h1 ) ',
+            '- 1.0 * P( p3 p4 h2 h1 => p3 p4 h1 h2 ) ',
+            '+ 1.0 * P( p3 p4 h2 h1 => p4 p3 h1 h2 ) '
+        ]
+    )
+    assert(
+        hl8.postfactors ==
+        ['Sum ( h6 p5 )', 't ( p5 p3 h6 h2 )', 'v ( h6 p4 h1 p5 )']
+    )
+    assert(str(hl8) == lines[7].replace('\n', ''))
+    cl8 = Cc4sLine(hl8)
+    assert(set(cl8.free_indices.split()) == set(["c", "d", "i", "j"]))
+    assert(cl8.summation_indices == " n e ")
+    assert(
+        cl8.prefactors ==
+        ['+ 1.0 ',
+        '- 1.0 * P( c d j i => d c j i ) ',
+        '- 1.0 * P( c d j i => c d i j ) ',
+        '+ 1.0 * P( c d j i => d c i j ) ']
+    )
+    assert(cl8.postfactors == ['Tabij["ecnj"]', 'Viajb["ndie"]'])
+    assert(
+        cl8.to_cpp() == [
+            '( + 1.0  ) * Tabij["ecnj"] * Viajb["ndie"];',
+            '( - 1.0  ) * Tabij["ednj"] * Viajb["ncie"];',
+            '( - 1.0  ) * Tabij["ecni"] * Viajb["ndje"];',
+            '( + 1.0  ) * Tabij["edni"] * Viajb["ncje"];'
+        ]
+    )
