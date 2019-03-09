a00cfa613291bc96d9471e2f29a1647343b04b75
Alejandro Gallo <aamsgallo@gmail.com>
2019-03-09 02:07:59 +0100

    Add more cisd

--- tests/test_cisd.py
+++ tests/test_cisd.py
@@ -21,6 +21,13 @@ def test_E():
         ['Sum ( h1 p2 )', 'f ( h1 p2 )', 't ( p2 h1 )']
     )
     assert(str(hl_singles) == lines[0].replace('\n', ''))
+    cl_singles = Cc4sLine(hl_singles)
+    assert(cl_singles.free_indices == " ")
+    assert(cl_singles.summation_indices == " i b ")
+    assert(cl_singles.prefactors == ["+ 1.0 "])
+    assert(cl_singles.postfactors == ['Fia["ib"]', 'Tai["bi"]'])
+    assert(cl_singles.to_cpp() == ['( + 1.0  ) * Fia["ib"] * Tai["bi"];'])
+
 
     assert(hl_doubles.free_indices == "")
     assert(hl_doubles.summation_indices == " h3 h4 p1 p2 ")
@@ -30,4 +37,11 @@ def test_E():
         ['Sum ( h3 h4 p1 p2 )', 't ( p1 p2 h3 h4 )', 'v ( h3 h4 p1 p2 )']
     )
     assert(str(hl_doubles) == lines[1].replace('\n', ''))
+    cl_doubles = Cc4sLine(hl_doubles)
+    assert(cl_doubles.summation_indices == " k l a b ")
+    assert(cl_doubles.prefactors == ["+ 0.25 "])
+    assert(cl_doubles.postfactors == ['Tabij["abkl"]', 'Vijab["klab"]'])
+    assert(
+        cl_doubles.to_cpp() == ['( + 0.25  ) * Tabij["abkl"] * Vijab["klab"];']
+    )
 
